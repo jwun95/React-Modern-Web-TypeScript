0501ef315a66dd8a901e082ce8b7df98b0c21964
Dartz <jwun95@naver.com>
2023-03-04 17:11:24 +0900

    useMemo & useCallback hooks 구현

--- ch04/src/pages/Callback.tsx
+++ ch04/src/pages/Callback.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import {useMemo, useCallback} from 'react'
+import {Title} from '../components'
+import {Button} from '../theme/daisyui'
+import * as D from '../data'
 
 export default function Callback() {
-	return (
-		<div>Callback</div>
-	)
+  const onClick = useCallback(() => alert('button clicked'), [])
+
+  const buttons = useMemo(
+    () =>
+      D.makeArray(3)
+        .map(D.randomName)
+        .map((name, index) => (
+          <Button
+            key={index}
+            onClick={onClick}
+            className="normal-case btn bton-primary btn-wide btn-xs">
+            {name}
+          </Button>
+        )),
+    [onClick]
+  )
+
+  return (
+    <div className="mt-4">
+      <Title>Callback</Title>
+      <div className="flex mt-4 justify-evenly">{buttons}</div>
+    </div>
+  )
 }
